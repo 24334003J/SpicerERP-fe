82f7593bda27ce77d59dc5409c04f9c494a1f8e2
24334003J <24334003@sau.edu.in>
2026-09-24 14:31:18 +0530

    Fixed register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -50,7 +50,7 @@ function Register() {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://spicererp-be-4.onrender.com/api/register",
         formData
       );
 
